d17024e54f8239885d93389c11fc53d9ee612c42
caspencer <caspencer@users.noreply.github.com>
2024-09-13 18:01:18 -0400

    add delay_ns, delay_us, delay_ms functions

--- include/delay.h
+++ include/delay.h
@@ -1,10 +1,30 @@
 #ifndef DELAY_H
 #define DELAY_H
 
+#include <stdint.h>
+
 /**
  * @brief Generates a delay by performing a busy-wait loop.
- * @param[in] count The number of iterations for the busy-wait loop.
+ * @param[in] count The number of iterations.
  */
 void delay(int count);
+
+/**
+ * @brief Generates a delay by performing a busy-wait loop.
+ * @param[in] count The number of nanoseconds to wait, must be at least 125. 
+ */
+void delay_ns(uint32_t val);
+
+/**
+ * @brief Generates a delay by performing a busy-wait loop.
+ * @param[in] count The number of microseconds to wait.
+ */
+void delay_us(uint32_t val);
+
+/**
+ * @brief Generates a delay by performing a busy-wait loop.
+ * @param[in] count The number of milliseconds to wait.
+ */
+void delay_ms(uint32_t val);
 
 #endif  // DELAY_H
--- src/delay.c
+++ src/delay.c
@@ -1,6 +1,21 @@
 #include "delay.h"
+#include <stdint.h>
 
-void delay(int count)
-{
+void delay(int count) {
     for (volatile int i = 0; i < count; i++);
+}
+
+void delay_ns(uint32_t val) {
+    uint32_t count = val * 0.008;
+    for (volatile uint32_t i = 0; i < count; i++);
+}
+
+void delay_us(uint32_t val) {
+    uint32_t count = val << 3;
+    for (volatile uint32_t i = 0; i < count; i++);
+}
+
+void delay_ms(uint32_t val) {
+    uint32_t count = (val * 1000) << 3;
+    for (volatile uint32_t i = 0; i < count; i++); 
 }
